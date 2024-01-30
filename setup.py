3108e6fe4a2f71529eb04a1ea38abaa489c9729b
doodlefisch <150934385+doodlefisch@users.noreply.github.com>
2024-01-30 10:26:57 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Quiz-game}',
+ version='1.0',
+ author='{Romain_Giraud}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
